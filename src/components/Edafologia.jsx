ad2ce536a3a87de2817fa1b8258ac99bd87389ed
Adrian Torres <92758447+adriantorresl@users.noreply.github.com>
2025-10-09 14:11:59 -0600

    Edafologia center -95.96711

--- src/components/Edafologia.jsx
+++ src/components/Edafologia.jsx
@@ -950,7 +950,7 @@ const MapView = () => {
 
   return (
     <MapContainer
-      center={[16.67566, -96.28311]}
+      center={[16.67566, -95.96711]}
       zoom={10}
       scrollWheelZoom={true}
       dragging={false}
